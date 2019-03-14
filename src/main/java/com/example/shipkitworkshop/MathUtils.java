d214ea1d6a1d0d5a3eab333561937ea399ffcc22
Marcin Stachniuk <mstachniuk@gmail.com>
2019-03-14 10:58:31 +0100

    Add BigInteger support

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -1,6 +1,7 @@
 package com.example.shipkitworkshop;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class MathUtils {
 
@@ -23,6 +24,12 @@ public class MathUtils {
     }
 
     public static boolean isTriangle(BigDecimal a, BigDecimal b, BigDecimal c) {
+        return a.add(b).compareTo(c) > 0
+                && a.add(c).compareTo(b) > 0
+                && b.add(c).compareTo(a) > 0;
+    }
+
+    public static boolean isTriangle(BigInteger a, BigInteger b, BigInteger c) {
         return a.add(b).compareTo(c) > 0
                 && a.add(c).compareTo(b) > 0
                 && b.add(c).compareTo(a) > 0;
