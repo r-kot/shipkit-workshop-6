be016189de7a858e45510ea165a28ffda3c8ed40
Marcin Stachniuk <mstachniuk@gmail.com>
2019-03-14 10:58:21 +0100

    Add BigDecimal support

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -1,5 +1,7 @@
 package com.example.shipkitworkshop;
 
+import java.math.BigDecimal;
+
 public class MathUtils {
 
     public static boolean isTriangle(int a, int b, int c) {
@@ -20,4 +22,9 @@ public class MathUtils {
                 && b + c > a;
     }
 
+    public static boolean isTriangle(BigDecimal a, BigDecimal b, BigDecimal c) {
+        return a.add(b).compareTo(c) > 0
+                && a.add(c).compareTo(b) > 0
+                && b.add(c).compareTo(a) > 0;
+    }
 }
